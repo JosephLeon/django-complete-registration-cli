656e86d24a45e9f2910e69c4c43f2ce92d0b2452
Joseph Leon <joseph@poeticsystems.com>
2014-11-27 18:38:10 +0200

    Testing command line functions and arguments.

--- createregistration.py
+++ createregistration.py
@@ -2,9 +2,20 @@ from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+            parser.add_argument('app_name')
+
     def handle(self, *args, **options):
         test = 'Command worked'
+        # argument = options['app_name']
+        # self.stdout.write(options)
+        print options
         try:
-            print test
+
+            # print argument
+            # print test
+            self.stdout.write(test)
+            # print app_name
         except test.DoesNotExist:
             raise CommandError("Test doesn't exist.")
